ac0541598174937d07c3f633ced91cc2800e03a2
Hammam-jeddawi <hammam.f.aljeddawi@outlook.com>
2024-06-25 06:13:18 +0300

    Update Find Max and Min in Array 1 .php
     // @param array $array The input array of integers.
     // @return array An associative array containing keys 'max' and 'min' with the
     // corresponding maximum and minimum values in the array.
     // @throws InvalidArgumentException If the input array is empty.

--- Find Max and Min in Array 1 .php
+++ Find Max and Min in Array 1 .php
@@ -7,10 +7,7 @@
   <body>
     <?php
 
- // @param array $array The input array of integers.
- // @return array An associative array containing keys 'max' and 'min' with the
- // corresponding maximum and minimum values in the array.
- // @throws InvalidArgumentException If the input array is empty.
+
 
 function findMaxMin(array $array): array
 {
